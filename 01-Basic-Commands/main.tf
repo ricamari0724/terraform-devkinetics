a07fd7d93ee3a70d6824bf58a0965a648c72f502
Rica Ferrancullo <131279889+ricamari0724@users.noreply.github.com>
2025-02-21 13:01:12 +0000

    Changes made after terraform plan and apply for No1

--- 01-Basic-Commands/main.tf
+++ 01-Basic-Commands/main.tf
@@ -1,22 +1,22 @@
 terraform {
   required_providers {
     docker = {
-        source = "kreuzwerker/docker"
-        version = "3.0.1"
+      source  = "kreuzwerker/docker"
+      version = "3.0.1"
     }
   }
 }
 
-provider "docker" {} 
+provider "docker" {}
 
-resource "docker_image" "nginx_image" { 
+resource "docker_image" "nginx_image" {
   name = "nginx:1.23.3"
 }
 
-resource "docker_container" "nginx_container" { 
-  name  = "web-server"                          
-  image = docker_image.nginx_image.image_id     
-  ports {                                       
-    internal = 80                               
+resource "docker_container" "nginx_container" {
+  name  = "web-server"
+  image = docker_image.nginx_image.image_id
+  ports {
+    internal = 80
   }
 }
